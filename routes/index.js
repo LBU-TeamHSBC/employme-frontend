71fe414b625f57b0756a236317e55591690fac29
Jack Chapman <jackchapman1997@hotmail.co.uk>
2018-03-14 17:00:21 +0000

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+res.render('index', { title: 'User Sign In', condition: true, user: ["userA", "userB", "userC", "userD"]});
 });
+
+router.get('/users/:uid', function(req, res, next) {
+ res.render('index')  
+});
+
 
 module.exports = router;
